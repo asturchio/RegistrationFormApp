70c8708e3d7bf507ed346da9e8ddcbcfb7b60d26
asturchio <adamsturchio@gmail.com>
2022-04-23 21:20:14 -0400

    initial commit

---
+++ app/src/main/java/com/asturchio/registrationformapp/User.kt
@@ -0,0 +1,15 @@
+package com.asturchio.registrationformapp
+
+import java.io.Serializable
+
+data class User(
+    val title: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val phone: String,
+    val password: String
+): Serializable{
+
+    fun getFullName() = "$title, $firstName, $lastName"
+}
